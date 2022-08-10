7546894f1e0fdbef59df125544f5d44195f273e0
Kelly Lee <amorfati38@gmail.com>
2022-08-09 23:11:25 -0600

    inheritance and polymorphism

--- ClassesAndRecords/Program.cs
+++ ClassesAndRecords/Program.cs
@@ -17,6 +17,18 @@ var (name, author) = classroomCourse;
 Console.WriteLine(name);
 Console.WriteLine(author);
 
+// Inheritance and Polymorphism
+CourseRecord timedCourse = new TimedCourseRecord(
+    "Working with C# Records",
+    "Roland Guijt",
+    Duration: 90
+    );
+
+var clonedCourse = timedCourse with { Name = "Inheritance and Polymorphism", Author = "John Taylor" };
+var castCourse = (TimedCourseRecord)timedCourse;
+
+Console.WriteLine(castCourse.Duration);
+
 public class Course
 {
     public string? Name { get; set; }
@@ -24,6 +36,7 @@ public class Course
 }
 
 public record CourseRecord(string Name, string Author);
+public record TimedCourseRecord(string Name, string Author, int Duration): CourseRecord(Name, Author);
 
 // Using a mix of positional and properties in the body
 public record MixedCourseRecord(string Name)
